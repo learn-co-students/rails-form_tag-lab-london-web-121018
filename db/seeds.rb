3d1eba22158b3f713642fbd12e7120867831bfc1
amaan19 <amaan.ahmed3@gmail.com>
2019-01-14 07:58:13 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Amaan", last_name: "ahmed")
+Student.create(first_name: "Anderson", last_name: "Coelho")
